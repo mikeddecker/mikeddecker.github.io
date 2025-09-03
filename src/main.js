49212db4e5501db34bb490f73c1a8a29426e32f6
Mike De Decker <mikeddecker@hotmail.com>
2025-09-03 18:17:22 +0200

    fresh start

--- src/main.js
+++ src/main.js
@@ -1,33 +1,5 @@
-import './assets/base.css'
-import './assets/tailwind.css'
-
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import PrimeVue from 'primevue/config'
-import Aura from '@primeuix/themes/aura'
-
+import './style.css'
 import App from './App.vue'
-import router from './router'
 
-import Button from 'primevue/button'
-
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-app.use(PrimeVue, {
-  theme: {
-    preset: Aura,
-    options: {
-      cssLayer: {
-        name: 'primevue',
-        order: 'theme, base, primevue',
-      },
-      darkModeSelector: '.my-app-dark',
-    },
-  },
-})
-
-app.component('Button', Button)
-
-app.mount('#app')
+createApp(App).mount('#app')
