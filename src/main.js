01016f881a1ea5a3644cad99e8ac55e37933a5a8
Mike De Decker <mikeddecker@hotmail.com>
2025-09-03 17:35:40 +0200

    change to vue/primevue/tailwind site

---
+++ src/main.js
@@ -0,0 +1,33 @@
+import './assets/base.css'
+import './assets/tailwind.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+
+import App from './App.vue'
+import router from './router'
+
+import Button from 'primevue/button'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      cssLayer: {
+        name: 'primevue',
+        order: 'theme, base, primevue',
+      },
+      darkModeSelector: '.my-app-dark',
+    },
+  },
+})
+
+app.component('Button', Button)
+
+app.mount('#app')
